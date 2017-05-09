2db2b62218e3f156d06233d9f50dee5811e88f9e
Martin Stein <loveablelobster@fastmail.fm>
2017-05-09 21:09:55 +0200

    improvements to routes
    Signed-off-by: Martin Stein <loveablelobster@fastmail.fm>

--- app/controllers/collection_objects_controller.rb
+++ app/controllers/collection_objects_controller.rb
@@ -4,6 +4,10 @@ class CollectionObjectsController < ApplicationController
   end
 
   def show
-  	@collection_object = CollectionObject.find_by(catalog_number: params[:id])
+    if params[:id] =~ /^NHMD-/
+  	  @collection_object = CollectionObject.find_by(catalog_number: params[:id])
+    else
+      @collection_object = CollectionObject.find(params[:id])
+    end
   end
 end
