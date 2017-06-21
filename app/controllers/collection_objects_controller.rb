26fd1dd8c539b3a25313e22c261a20a860e2d5d5
Martin Stein <loveablelobster@fastmail.fm>
2017-06-21 15:36:33 +0200

    towards RDF generation for CollectionObject
    first steps towards a collection_objects_controller method to generate a RDF representation

--- app/controllers/collection_objects_controller.rb
+++ app/controllers/collection_objects_controller.rb
@@ -37,4 +37,27 @@ class CollectionObjectsController < ApplicationController
       'Date georeferenced' => @collection_object.locality[:georeferenced_date]
     }.delete_if { |_k, v| v.nil? }
   end
+
+  def rdf
+    @dc = RDF::Vocab::DC11
+    @dwc = RDF::Vocab::DWC
+    @geo = RDF::Vocab::GEO
+    @dwcc = RDF::Vocabulary.new('http://rs.tdwg.org/dwc/curatorial/')
+    PREFIXES = {
+      rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      xsi: 'http://www.w3.org/2001/XMLSchema-instance',
+      xs: 'http://www.w3.org/2001/XMLSchema',
+      dwc: 'http://rs.tdwg.org/dwc/terms/',
+      dc: 'http://purl.org/dc/terms/',
+      geo: 'http://www.w3.org/2003/01/geo/wgs84_pos#'
+    }
+    if params[:id] =~ /^NHMD-/
+  	  @collection_object = CollectionObject.find_by(catalog_number: params[:id])
+    else
+      @collection_object = CollectionObject.find(params[:id])
+    end
+    @collection_object.attributes.map do |field|
+			[@dwc[field.first.camelize(:lower)], field.last]
+		end
+  end
 end
