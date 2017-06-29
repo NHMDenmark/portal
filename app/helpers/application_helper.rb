1e77f15875c91e64f1692b152918348f47745071
Martin Stein <loveablelobster@fastmail.fm>
2017-06-29 18:08:26 +0200

    tidy_up helper to prepare attributes for output
    removes mongo IDs and blanks from model object attributes
    also humanizes the keys
    
    Signed-off-by: Martin Stein <loveablelobster@fastmail.fm>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # returns hash with mongoid IDs and blank keys removed
+  def tidy_up(hash)
+    hash.reject { |_k, v| v.is_a? BSON::ObjectId }
+				.delete_if { |_k, v| v.blank? }
+				.map { |k, v| [k.to_s.humanize, v] }
+				.to_h
+  end
 end
