c23e4c50150f7812a5ca3124ffe63da87034f3e7
Martin Stein <loveablelobster@fastmail.fm>
2017-06-30 16:52:02 +0200

    use OpenTopoMap for tiles
    https://opentopomap.org/
    Signed-off-by: Martin Stein <loveablelobster@fastmail.fm>

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,6 @@
-Leaflet.tile_layer = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-Leaflet.attribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+# Leaflet.tile_layer = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+Leaflet.tile_layer = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+# Leaflet.attribution = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+Leaflet.attribution = 'Map data © <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org/">SRTM</a> | Map style © <a href="https://opentopomap.org/">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+
 Leaflet.max_zoom = 18
