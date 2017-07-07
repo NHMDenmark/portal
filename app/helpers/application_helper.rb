912cfff31246444ecd8a06663d1c674f38d71443
Martin Stein <loveablelobster@fastmail.fm>
2017-07-07 12:26:12 +0200

    partial renders other_catalog_numbers collection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # returns hash with mongoid IDs and blank keys removed
   def tidy_up(hash)
-    hash.reject { |_k, v| v.is_a?(BSON::ObjectId) || v.is_a?(Hash) }
+    hash.reject { |_k, v| v.is_a?(BSON::ObjectId) || v.is_a?(Hash) || v.is_a?(Array) }
 				.delete_if { |_k, v| v.blank? }
 				.map { |k, v| [k.to_s.humanize, v] }
 				.to_h
