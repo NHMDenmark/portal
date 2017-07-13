7c922f075f14dcd094deec8cfd1f7f355e499fd8
Martin Stein <loveablelobster@fastmail.fm>
2017-07-13 12:32:08 +0200

    moved DwcTaxon attribute rendering to partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,22 @@
 module ApplicationHelper
+
+  def de_namespace(field_name)
+    namespace, *term = field_name.split('_')
+  end
+
+  def namespace(field_name)
+    de_namespace(field_name).first
+  end
+
+  def term(field_name)
+    de_namespace(field_name).last
+  end
+
   # returns hash with mongoid IDs and blank keys removed
   def tidy_up(hash)
     hash.reject { |_k, v| v.is_a?(BSON::ObjectId) || v.is_a?(Hash) || v.is_a?(Array) }
 				.delete_if { |_k, v| v.blank? }
-				.map { |k, v| [k.to_s.humanize, v] }
+				.map { |k, v| [k, v] }
 				.to_h
   end
 end
