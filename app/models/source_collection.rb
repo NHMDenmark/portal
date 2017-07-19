4e7d6c80bc1d7d94857bb7674b27fddb95dea850
Martin Stein <loveablelobster@fastmail.fm>
2017-07-19 13:16:07 +0200

    prefixed SourceCollection fields with dwc_
    as namespace token

--- app/models/source_collection.rb
+++ app/models/source_collection.rb
@@ -1,8 +1,8 @@
 class SourceCollection
   include Mongoid::Document
   has_many :collection_objects
-  field :collection_id, type: String
-  field :collection_code, type: String
-  field :institution_name, type: String
-  field :institution_code, type: String
+  field :dwc_collection_id, type: String
+  field :dwc_collection_code, type: String
+  field :dwc_institution_name, type: String
+  field :dwc_institution_code, type: String
 end
