7c852df5a06a3aec82a68fcfb763b0b49912db36
Martin Stein <loveablelobster@fastmail.fm>
2017-08-17 12:03:35 +0200

    generating section headers in co detail views
    auto-generating section headers in views/collection_objects/_record_details.html.erb partial with a section_header method in helpers/collection_objects_helper.rb
    
    first implementation using hash of interpolated string literals for each section, this could be moved to a YAML config file

--- app/helpers/collection_objects_helper.rb
+++ app/helpers/collection_objects_helper.rb
@@ -1,10 +1,10 @@
 module CollectionObjectsHelper
-  def display_order(record_detail)
+  def display_order(record_details)
     order = %w(taxon identification geological_context location event organism)
 
     # insert 'metadata' as the last element
     order << 'metadata'
-    record_detail.sort do |a, b|
+    record_details.sort do |a, b|
       order.index(a[:section]) <=> order.index(b[:section])
     end
   end
@@ -15,5 +15,17 @@ module CollectionObjectsHelper
 
     italics = %w(dwc_genus dwc_subgenus dwc_specific_epithet dwc_infraspecific_epithet)
     italics.include?(term)
+  end
+
+  def section_header(section, record_details)
+    header_strings = {
+      'taxon' => "#{record_details[:dwc_scientific_name]}",
+      'identification' => "#{record_details[:dwc_type_status]}",
+      'geological_context' => "Geological Context",
+      'location' => "#{record_details[:dwc_higher_geography]}",
+      'event' => "#{record_details[:dwc_event_date]}",
+      'organism' => "Organism"
+    }
+    header_strings[section]
   end
 end
