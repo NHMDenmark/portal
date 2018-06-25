933a073c6e3b56bb27b59c452633c73b299e39cd
Martin Stein <loveablelobster@fastmail.fm>
2018-06-25 11:12:58 +0200

    inverted aliases on CO
    vocab-prefixed terms are now aliases, shorter names as used in portal are proper field names

--- app/models/collection_object.rb
+++ app/models/collection_object.rb
@@ -22,69 +22,69 @@ class CollectionObject
   # An identifier for the Occurrence (as opposed to a particular digital record
   # of the occurrence).
   # The PURL.
-  field :dwc_occurrence_id, as: :stable_identifier, type: String
+  field :stable_identifier, as: :dwc_occurrence_id, type: String
 
   # An identifier (preferably unique) for the record within the data set or
   # collection.
-  field :dwc_catalog_number, as: :catalog_number, type: String
+  field :catalog_number, as: :dwc_catalog_number, type: String
   field :_id, type: String, overwrite: true, default: -> { dwc_catalog_number }
 
   # An identifier given to the Occurrence at the time it was recorded.
   # Often serves as a link between field notes and an Occurrence record,
   # such as a specimen collector's number.
-  field :dwc_record_number, as: :field_number, type: String
+  field :field_number, as: :dwc_record_number, type: String
 
   # A list (concatenated and separated) of names of people, groups, or
   # organizations responsible for recording the original Occurrence.
   # The primary collector or observer, especially one who applies a personal
   # identifier (recordNumber), should be listed first.
   # TODO: move to embedded model (embeds_many), to allow semantic enrichment.
-  field :dwc_recorded_by, as: :collector_name, type: String
+  field :collector_name, as: :dwc_recorded_by, type: String
 
   # The number of individuals represented present at the time of the Occurrence.
-  field :dwc_individual_count, as: :individual_count, type: Integer
+  field :individual_count, as: :dwc_individual_count, type: Integer
 
   # A number or enumeration value for the quantity of organisms.
-  field :dwc_organism_quantity, as: :organism_quantity, type: Float
+  field :organism_quantity, as: :dwc_organism_quantity, type: Float
 
   # The type of quantification system used for the quantity of organisms.
-  field :dwc_organism_quantity_type, as: :organism_quantity_type, type: String
+  field :organism_quantity_type, as: :dwc_organism_quantity_type, type: String
 
   # The sex of the biological individual(s) represented in the Occurrence.
-  field :dwc_sex, as: :sex, type: String
+  field :sex, as: :dwc_sex, type: String
 
   # The age class or life stage of the biological individual(s) at the time the
   # Occurrence was recorded.
-  field :dwc_life_stage, as: :life_stage, type: String
+  field :life_stage, as: :dwc_life_stage, type: String
 
   # The reproductive condition of the biological individual(s) represented in
   # the Occurrence.
-  field :dwc_reproductive_condition, as: :reproductive_condition, type: String
+  field :reproductive_condition, as: :dwc_reproductive_condition, type: String
 
   # A description of the behavior shown by the subject at the time the
   # Occurrence was recorded.
-  field :dwc_behavior, as: :behavior, type: String
+  field :behavior, as: :dwc_behavior, type: String
 
   # The process by which the biological individual(s) represented in the
   # Occurrence became established at the location.
-  field :dwc_establishment_means, as: :establishment_means, type: String
+  field :establishment_means, as: :dwc_establishment_means, type: String
 
   # A statement about the presence or absence of a Taxon at a Location.
-  field :dwc_occurrence_status, as: :occurrence_status, type: String
+  field :occurrence_status, as: :dwc_occurrence_status, type: String
 
   # A list (concatenated and separated) of preparations and preservation methods
   # for a specimen.
   # TODO: move to embedded model (embeds_many).
-  field :dwc_preparations, as: :preparations, type: String
+  field :preparations, as: :dwc_preparations, type: String
 
   # The current state of a specimen with respect to the collection identified in
   # collectionCode or collectionID.
-  field :dwc_disposition, as: :disposition, type: String
+  field :disposition, as: :dwc_disposition, type: String
 
   # Comments or notes about the Occurrence.
-  field :dwc_occurrence_remarks, as: :occurrence_remakrs, type: String
+  field :remarks, as: :dwc_occurrence_remarks, type: String
 
-  index({ dwc_catalog_number: 1 }, unique: true)
+  index({ catalog_number: 1 }, unique: true)
 
   def self.quicksearch_fields
     ['dwc_recorded_by', 'dwc_identification.dwc_type_status',
