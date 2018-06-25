325bd0f797f6952ce7f533f24bc16d8969bb12ac
Martin Stein <loveablelobster@fastmail.fm>
2018-06-25 10:25:43 +0200

    model fields explained
    comments inserted from DwC definitions

--- app/models/collection_object.rb
+++ app/models/collection_object.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# An existence of an Organism (sensu http://rs.tdwg.org/dwc/terms/Organism)
+# at a particular place at a particular time.
 class CollectionObject
   include Mongoid::Document
 
@@ -15,34 +19,72 @@ class CollectionObject
   embeds_one :dwc_taxon
   embeds_one :metadata
 
-  field :mapping_id, type: Integer # created by the Specify Exporter
+  # An identifier for the Occurrence (as opposed to a particular digital record
+  # of the occurrence).
+  # The PURL.
+  field :dwc_occurrence_id, type: String
 
-  field :dwc_occurrence_id, type: String                    # persistent identifier
-  field :dwc_material_sample_id, type: String               # is normally part of DwC class of its own
-
+  # An identifier (preferably unique) for the record within the data set or
+  # collection.
   field :dwc_catalog_number, type: String
-  field :_id, type: String, overwrite: true, default: ->{ dwc_catalog_number } # maybe use the occurrence_id;  overwrite => true suppresses the mongo overwrite warning
+  field :_id, type: String, overwrite: true, default: -> { dwc_catalog_number }
 
+  # An identifier given to the Occurrence at the time it was recorded.
+  # Often serves as a link between field notes and an Occurrence record,
+  # such as a specimen collector's number.
   field :dwc_record_number, type: String
-  field :dwc_recorded_by, type: String
+
+  # A list (concatenated and separated) of names of people, groups, or
+  # organizations responsible for recording the original Occurrence.
+  # The primary collector or observer, especially one who applies a personal
+  # identifier (recordNumber), should be listed first.
+  # TODO: move to embedded model (embeds_many), to allow semantic enrichment.
+  field :dwc_recorded_by, as: :collector_name, type: String
+
+  # The number of individuals represented present at the time of the Occurrence.
   field :dwc_individual_count, type: Integer
+
+  # A number or enumeration value for the quantity of organisms.
   field :dwc_organism_quantity, type: Float
+
+  # The type of quantification system used for the quantity of organisms.
   field :dwc_organism_quantity_type, type: String
+
+  # The sex of the biological individual(s) represented in the Occurrence.
   field :dwc_sex, type: String
+
+  # The age class or life stage of the biological individual(s) at the time the
+  # Occurrence was recorded.
   field :dwc_life_stage, type: String
+
+  # The reproductive condition of the biological individual(s) represented in
+  # the Occurrence.
   field :dwc_reproductive_condition, type: String
+
+  # A description of the behavior shown by the subject at the time the
+  # Occurrence was recorded.
   field :dwc_behavior, type: String
+
+  # The process by which the biological individual(s) represented in the
+  # Occurrence became established at the location.
   field :dwc_establishment_means, type: String
+
+  # A statement about the presence or absence of a Taxon at a Location.
   field :dwc_occurrence_status, type: String
-  field :dwc_preparations, type: String                     # should be embeds_many
+
+  # A list (concatenated and separated) of preparations and preservation methods
+  # for a specimen.
+  # TODO: move to embedded model (embeds_many).
+  field :dwc_preparations, type: String
+
+  # The current state of a specimen with respect to the collection identified in
+  # collectionCode or collectionID.
   field :dwc_disposition, type: String
+
+  # Comments or notes about the Occurrence.
   field :dwc_occurrence_remarks, type: String
 
-  index({ dwc_catalog_number: 1 }, { unique: true })
-
-  # Organism class terms
-
-  # MaterialSample/LivingSpecimen/PreservedSpecimen/FossilSpecimen class terms
+  index({ dwc_catalog_number: 1 }, unique: true)
 
   def self.quicksearch_fields
     ['dwc_recorded_by', 'dwc_identification.dwc_type_status',
